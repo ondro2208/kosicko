ddf2ee8a457e5018373b13c8a14ce3536973a243
Matúš Ladomirják <matus.ladomirjak@student.tuke.sk>
2016-11-26 13:31:00 +0100

    matus push

--- app/src/main/java/com/hackathon/kosicko/StartActivity.java
+++ app/src/main/java/com/hackathon/kosicko/StartActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class StartActivity extends AppCompatActivity {
 ////changes by matt
     //changes by petos
+    //fdfd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
