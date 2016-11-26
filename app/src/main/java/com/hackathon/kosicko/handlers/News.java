6a9f5f00a1c59ef3aa1ff71773f57fb668f15a8f
Peter Šabak <peter.sabak@student.tuke.sk>
2016-11-26 21:16:29 +0100

    psabak - NewsActivity modified

---
+++ app/src/main/java/com/hackathon/kosicko/handlers/News.java
@@ -0,0 +1,68 @@
+package com.hackathon.kosicko.handlers;
+
+/**
+ * Created by Peťo on 26.11.2016.
+ */
+
+public class News {
+
+    //Private variables
+    private String source;
+    private String title;
+    private String image;
+    private String url;
+    private String date;
+
+    //empty constructor
+    public News(){
+
+    }
+
+    public News(String source, String title, String image, String url, String date) {
+        this.source = source;
+        this.title = title;
+        this.image = image;
+        this.url = url;
+        this.date = date;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
