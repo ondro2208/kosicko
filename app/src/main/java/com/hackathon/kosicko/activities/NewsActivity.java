283211dc2cff1958b3c1f20e97f5b6328403a700
Peter Šabak <peter.sabak@student.tuke.sk>
2016-11-26 16:06:32 +0100

    - pridané EmptyActivity - konkrétne NewsActivity

---
+++ app/src/main/java/com/hackathon/kosicko/activities/NewsActivity.java
@@ -0,0 +1,15 @@
+package com.hackathon.kosicko.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hackathon.kosicko.R;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
