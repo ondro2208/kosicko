eb5d7c5c6f83bab417be6c14660b9c807cd470b4
Matúš Ladomirják <matus.ladomirjak@student.tuke.sk>
2016-11-26 16:12:10 +0100

    matus push

--- app/src/main/java/com/hackathon/kosicko/activities/StartActivity.java
+++ app/src/main/java/com/hackathon/kosicko/activities/StartActivity.java
@@ -1,17 +1,19 @@
 package com.hackathon.kosicko.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.hackathon.kosicko.R;
 
+
 public class StartActivity extends AppCompatActivity {
-////changes by matt
-    //changes by petos
-    //fdfd
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+//        Intent beer=new Intent(this,BeerActivity.class);
+//        startActivity(beer);
     }
 }
