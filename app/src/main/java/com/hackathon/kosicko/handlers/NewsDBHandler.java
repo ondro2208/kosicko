a327d99974f81ae872ecd9b5cbeba827fc9e3064
Peter Šabak <peter.sabak@student.tuke.sk>
2016-11-27 02:38:11 +0100

    psabak - pridane fejkove data aby sme videli ako bude vyzerat NewsActivity

--- app/src/main/java/com/hackathon/kosicko/handlers/NewsDBHandler.java
+++ app/src/main/java/com/hackathon/kosicko/handlers/NewsDBHandler.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Peťo on 26.11.2016.
